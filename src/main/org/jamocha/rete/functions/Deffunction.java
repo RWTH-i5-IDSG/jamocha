aaef354e6001a857f3cd3e94550e80e88552677b
woolfel <woolfel@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-01-25 01:36:49 +0000

    I've started to work on deffunction. it will take a few weeks to implement it. it probably won't make it into the first release, but it's good to start working on it now. peter
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@447 2cd91b7d-3b21-0410-a5f4-f42db4f24360

---
+++ src/main/org/jamocha/rete/functions/Deffunction.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2002-2007 Peter Lin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://ruleml-dev.sourceforge.net/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.jamocha.rete.functions;
+
+import org.jamocha.rete.Function;
+import org.jamocha.rete.Parameter;
+import org.jamocha.rete.Rete;
+import org.jamocha.rete.ReturnVector;
+
+/**
+ * Deffunction is used for functions that are declared in the
+ * shell. It is different than a function written in java.
+ * Deffunction run interpreted and are mapped to existing
+ * functions.
+ * 
+ * @author Peter Lin
+ */
+public class Deffunction implements Function {
+
+    protected String name = null;
+    protected String ppString = null;
+    protected Parameter[] parameters = null;
+    protected Function functions = null;
+    protected Class[] functionParams = null;
+    protected int returnType;
+    
+    /**
+     * 
+     */
+    public Deffunction() {
+    }
+
+    public ReturnVector executeFunction(Rete engine, Parameter[] params) {
+        return null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public Class[] getParameter() {
+        return this.functionParams;
+    }
+
+    public int getReturnType() {
+        return this.returnType;
+    }
+
+    public void setPPString(String text) {
+        this.ppString = text;
+    }
+    
+    public String toPPString(Parameter[] params, int indents) {
+        return this.ppString;
+    }
+
+}
