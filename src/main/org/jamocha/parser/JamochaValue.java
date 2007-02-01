903ae438adbb54d3702ade301cb3f9a07b1e1df1
woolfel <woolfel@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-02-01 00:35:15 +0000

    manage to get manners128 down to 20980 by simplifying the equals method. it's over kill to do all the extra steps. this slight change improved the performance 4%.
    the only other thing to do is to simplify the hashcode method to simply return the hashcode of type and value, without doing the extra multiplication against the prime.
    that should improve it another 2-3%. beyond that, I can't think of anything else to simplify.
    
    peter
    
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@503 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/parser/JamochaValue.java
+++ src/main/org/jamocha/parser/JamochaValue.java
@@ -296,22 +296,15 @@ public class JamochaValue {
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		final JamochaValue other = (JamochaValue) obj;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
+            return true;
+        if (obj == null)
+            return false;
+        final JamochaValue other = (JamochaValue) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
 	}
 }
