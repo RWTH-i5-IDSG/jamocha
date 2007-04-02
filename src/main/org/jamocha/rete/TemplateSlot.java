f48334ec83f77fd99d10795d0076cc11bb41d315
cemonds <cemonds@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-04-02 18:20:14 +0000

    - forgot to commit the TemplateSlot ;)
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@682 2cd91b7d-3b21-0410-a5f4-f42db4f24360

---
+++ src/main/org/jamocha/rete/TemplateSlot.java
@@ -0,0 +1,87 @@
+package org.jamocha.rete;
+
+import org.jamocha.parser.EvaluationException;
+import org.jamocha.parser.Expression;
+
+public class TemplateSlot extends Slot {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * node count is used to keep track of how many nodes use the given
+     * slot. This is done for statistical purposes, which serve 3 main
+     * functions.
+     * 1. provide a way to calculate the relative importance of a slot
+     * with regard to the entire RETE network
+     * 2. provide a way to optimize runtime execution
+     * 3. provide valuable information for engine management
+     */
+    private int nodeCount = 1;
+    
+    private boolean multiSlot = false;
+    
+    private Expression defaultExpression;
+
+    public TemplateSlot(String name) {
+	super(name);
+    }
+
+    public boolean isMultiSlot() {
+        return multiSlot;
+    }
+
+    public void setMultiSlot(boolean multiSlot) {
+        this.multiSlot = multiSlot;
+    }
+    
+    public Expression getDefaultExpression() {
+	return defaultExpression;
+    }
+
+    /**
+     * In some cases, a deftemplate can be define with a default value.
+     * 
+     * @param value
+     */
+    public void setDefaultExpression(Expression defaultExpression) {
+    	this.defaultExpression = defaultExpression;
+    }
+    
+    public Slot createSlot(Rete engine) throws EvaluationException {
+	Slot result = new Slot(getName());
+	result.setValueType(getValueType());
+	if(defaultExpression != null) {
+	    result.setValue(defaultExpression.getValue(engine));
+	}
+	return result;
+    }
+
+    /**
+     * return the number of nodes the given slot participates
+     * in. It may not be a complete count. In some cases, it
+     * may only count the direct successors of ObjectTypeNode
+     * @return
+     */
+    public int getNodeCount() {
+    	return this.nodeCount;
+    }
+
+    /**
+     * Increment the node count
+     */
+    public void incrementNodeCount() {
+    	this.nodeCount++;
+    }
+
+    /**
+     * decrement the node count
+     *
+     */
+    public void decrementNodeCount() {
+    	--this.nodeCount;
+    }
+
+}
