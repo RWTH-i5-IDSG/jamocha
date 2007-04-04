5c6ad16fa79c70fc5a700b37c1234a0fee256a5b
cemonds <cemonds@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-04-04 08:37:31 +0000

    - initial import of the CLIPS formatter
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@731 2cd91b7d-3b21-0410-a5f4-f42db4f24360

---
+++ src/main/org/jamocha/parser/clips/CLIPSFormatter.java
@@ -0,0 +1,112 @@
+package org.jamocha.parser.clips;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.jamocha.parser.Expression;
+import org.jamocha.parser.Formatter;
+import org.jamocha.parser.JamochaValue;
+import org.jamocha.rete.BoundParam;
+import org.jamocha.rete.ExpressionCollection;
+import org.jamocha.rete.FunctionParam2;
+import org.jamocha.rete.Slot;
+import org.jamocha.rete.SlotParam;
+
+public class CLIPSFormatter implements Formatter {
+
+    public String formatExpression(Expression expression) {
+	if (expression instanceof JamochaValue) {
+	    return formatJamochaValue((JamochaValue) expression);
+	} else if (expression instanceof FunctionParam2) {
+	    return formatFunctionParam(expression);
+	} else if (expression instanceof BoundParam) {
+	    return formatBoundParam(expression);
+	} else if (expression instanceof SlotParam) {
+	    return formatSlotParam(expression);
+	} else if (expression instanceof ExpressionCollection) {
+	    return formatExpressionCollection(expression);
+	}
+	return "";
+    }
+
+    private String formatExpressionCollection(Expression expression) {
+	// TODO Auto-generated method stub
+
+    }
+
+    private String formatSlotParam(Expression expression) {
+	// TODO Auto-generated method stub
+
+    }
+
+    private String formatBoundParam(Expression expression) {
+	// TODO Auto-generated method stub
+
+    }
+
+    private String formatFunctionParam(Expression expression) {
+	// TODO Auto-generated method stub
+
+    }
+
+    private String formatJamochaValue(JamochaValue jamochaValue) {
+	StringBuilder sb = new StringBuilder();
+	switch (jamochaValue.getType()) {
+	case NIL:
+	    return "NIL";
+	case STRING:
+	    return "\"" + jamochaValue.getStringValue() + "\"";
+	case FACT_ID:
+	    return "f-" + jamochaValue.getFactIdValue();
+	case DATETIME:
+	    GregorianCalendar c = (GregorianCalendar) jamochaValue.getDateValue();
+	    sb.append('"');
+	    sb.append(fillToFixedLength(c.get(Calendar.YEAR), "0", 4)).append('-');
+	    sb.append(fillToFixedLength(c.get(Calendar.MONTH) + 1, "0", 2)).append('-');
+	    sb.append(fillToFixedLength(c.get(Calendar.DAY_OF_MONTH), "0", 2)).append(' ');
+	    sb.append(fillToFixedLength(c.get(Calendar.HOUR_OF_DAY), "0", 2)).append(':');
+	    sb.append(fillToFixedLength(c.get(Calendar.MINUTE), "0", 2)).append(':');
+	    sb.append(fillToFixedLength(c.get(Calendar.SECOND), "0", 2));
+	    int gmtOffsetMillis = c.get(Calendar.ZONE_OFFSET);
+	    if (gmtOffsetMillis >= 0) {
+		sb.append('+');
+	    } else {
+		sb.append('-');
+	    }
+	    int gmtOffsetHours = gmtOffsetMillis / (1000 * 60 * 60);
+	    sb.append(fillToFixedLength(gmtOffsetHours, "0", 2));
+	    sb.append('"');
+	    break;
+	case LIST:
+	    sb.append('[');
+	    for (int i = 0; i < jamochaValue.getListCount(); ++i) {
+		if (i > 0) {
+		    sb.append(", ");
+		}
+		sb.append(formatJamochaValue(jamochaValue.getListValue(i)));
+	    }
+	    sb.append(']');
+	    break;
+	case SLOT:
+	    sb.append('(');
+	    Slot slot = jamochaValue.getSlotValue();
+	    sb.append(slot.getName());
+	    sb.append(' ');
+	    sb.append(formatJamochaValue(slot.getValue()));
+	    sb.append(')');
+	    break;
+
+	default:
+	    sb.append(jamochaValue.getObjectValue().toString());
+	    break;
+	}
+	return sb.toString();
+    }
+
+    private String fillToFixedLength(int val, String fill, int length) {
+	String res = String.valueOf(val);
+	while (res.length() < length)
+	    res = fill + res;
+	return res;
+    }
+}
