41c32d31c53046f161ade5bdf1d56a5e483f54fe
id864 <id864@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-04-17 16:13:55 +0000

    Moved all configuration classes to a new package. khk / sr
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@842 2cd91b7d-3b21-0410-a5f4-f42db4f24360

---
+++ src/main/org/jamocha/rete/configurations/AssertConfiguration.java
@@ -0,0 +1,49 @@
+
+
+package org.jamocha.rete.configurations;
+
+import org.jamocha.parser.EvaluationException;
+import org.jamocha.parser.JamochaValue;
+import org.jamocha.rete.Parameter;
+import org.jamocha.rete.Rete;
+
+public class AssertConfiguration implements Parameter {
+
+	
+	String templateName = null;
+	
+	SlotConfiguration[] slots = null;
+	
+	
+	public boolean isObjectBinding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getExpressionString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JamochaValue getValue(Rete engine) throws EvaluationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SlotConfiguration[] getSlots() {
+		return slots;
+	}
+
+	public void setSlots(SlotConfiguration[] slots) {
+		this.slots = slots;
+	}
+
+	public String getTemplateName() {
+		return templateName;
+	}
+
+	public void setTemplateName(String templateName) {
+		this.templateName = templateName;
+	}
+
+}
---
+++ src/main/org/jamocha/rete/configurations/SlotConfiguration.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.jamocha.rete.configurations;
+
+import org.jamocha.parser.EvaluationException;
+import org.jamocha.parser.JamochaValue;
+import org.jamocha.rete.Parameter;
+import org.jamocha.rete.Rete;
+
+/**
+ * @author Karl-Heinz Krempels, Sebastian Reinartz
+ *
+ */
+public class SlotConfiguration implements Parameter {
+
+	private String slotName = null;
+	
+	private Parameter[] slotValues = null;
+	
+	/* (non-Javadoc)
+	 * @see org.jamocha.rete.Parameter#isObjectBinding()
+	 */
+	public boolean isObjectBinding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jamocha.parser.Expression#getExpressionString()
+	 */
+	public String getExpressionString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.jamocha.parser.Expression#getValue(org.jamocha.rete.Rete)
+	 */
+	public JamochaValue getValue(Rete engine) throws EvaluationException {
+		//multislot:
+		if (isMultislot()){
+			JamochaValue[] jvs= new JamochaValue[slotValues.length];
+			for (int i=0 ; i< slotValues.length; i++){
+				jvs[i] = slotValues[i].getValue(engine);
+			}
+			return JamochaValue.newList(jvs);
+			
+		//single slot:	
+		}else {
+			return slotValues[0].getValue(engine);
+		}
+		
+	}
+
+	public String getSlotName() {
+		return slotName;
+	}
+
+	public void setSlotName(String slotName) {
+		this.slotName = slotName;
+	}
+
+	public Parameter[] getSlotValues() {
+		return slotValues;
+	}
+
+	public void setSlotValues(Parameter[] slotValues) {
+		this.slotValues = slotValues;
+	}
+
+	public Boolean isMultislot(){
+		return (slotValues.length >1);
+	}
+}
+
