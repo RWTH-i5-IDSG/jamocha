e26e2666d812aefd3bbb97cbb82bb1569fc09bd1
procole <procole@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-06-09 11:55:24 +0000

    sry, i've forgotten to add StopCompileException, since my Eclipse freezes from time to time for about a minute :(
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1163 2cd91b7d-3b21-0410-a5f4-f42db4f24360

---
+++ src/main/org/jamocha/rete/StopCompileException.java
@@ -0,0 +1,29 @@
+package org.jamocha.rete;
+
+public class StopCompileException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StopCompileException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StopCompileException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StopCompileException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StopCompileException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
