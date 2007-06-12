d5989ef3a33a9d3a781060efacd4f33bdb6e885e
id864 <id864@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-06-12 15:47:13 +0000

    generated a new sf parser.
    khk
    
    
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1171 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/parser/sfp/SFPParser.java
+++ src/main/org/jamocha/parser/sfp/SFPParser.java
@@ -5835,10 +5835,7 @@ Data Types:
   private int jj_gc = 0;
 
   public SFPParser(java.io.InputStream stream) {
-     this(stream, null);
-  }
-  public SFPParser(java.io.InputStream stream, String encoding) {
-    try { jj_input_stream = new SimpleCharStream(stream, encoding, 1, 1); } catch(java.io.UnsupportedEncodingException e) { throw new RuntimeException(e); }
+    jj_input_stream = new SimpleCharStream(stream, 1, 1);
     token_source = new SFPParserTokenManager(jj_input_stream);
     token = new Token();
     jj_ntk = -1;
@@ -5848,10 +5845,7 @@ Data Types:
   }
 
   public void ReInit(java.io.InputStream stream) {
-     ReInit(stream, null);
-  }
-  public void ReInit(java.io.InputStream stream, String encoding) {
-    try { jj_input_stream.ReInit(stream, encoding, 1, 1); } catch(java.io.UnsupportedEncodingException e) { throw new RuntimeException(e); }
+    jj_input_stream.ReInit(stream, 1, 1);
     token_source.ReInit(jj_input_stream);
     token = new Token();
     jj_ntk = -1;
@@ -6060,7 +6054,6 @@ Data Types:
   final private void jj_rescan_token() {
     jj_rescan = true;
     for (int i = 0; i < 17; i++) {
-    try {
       JJCalls p = jj_2_rtns[i];
       do {
         if (p.gen > jj_gen) {
@@ -6087,7 +6080,6 @@ Data Types:
         }
         p = p.next;
       } while (p != null);
-      } catch(LookaheadSuccess ls) { }
     }
     jj_rescan = false;
   }
