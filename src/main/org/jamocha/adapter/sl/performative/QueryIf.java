c26be4967b2b0640ced23630457ea3e0c2401ccd
musti-50 <musti-50@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-06-16 15:02:38 +0000

    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1187 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/adapter/sl/performative/QueryIf.java
+++ src/main/org/jamocha/adapter/sl/performative/QueryIf.java
@@ -27,10 +27,10 @@ import org.jamocha.parser.sl.SimpleNode;
  * This class walks through an SL code tree and translates it to CLIPS depending
  * on the given performative.
  * 
- * @author Alexander Wilden
+ * @author Mustafa Karafil
  * 
  */
-public class QueryIf {
+public class QueryIf extends SLPerformative{
 
 	/**
 	 * A private constructor to force access only in a static way.
@@ -40,8 +40,7 @@ public class QueryIf {
 	}
 
 	/**
-	 * Translates SL code of a request to CLIPS code. A request only contains
-	 * one action.
+	 * Translates SL code of a query-if to CLIPS code. 
 	 * 
 	 * @param slContent
 	 *            The SL content we have to translate.
@@ -63,108 +62,15 @@ public class QueryIf {
 					"Could not translate from SL to CLIPS.", e);
 		}
 		// Walk through the children until we have something useful
-		sn = getChildAtLevel(sn, 3);
-		if (sn.getID() == SLParserTreeConstants.JJTATOMICFORMULA) {
-			// Here we have an AtomicFormula
-			String predicateName = getChild(sn, 0).getText();
-			
-			String lastAssert = "";
-            for(int i = 1; i<sn.jjtGetNumChildren(); i++){
-            	lastAssert = lastAssert + " ("+resolveParameters(getChild(sn,i))+") "; 
-            }
-		    result.append("(defrule \"query-if\""+lastAssert+"=>"+
-		    		      " (return Boolean ("+predicateName+lastAssert +")))");
+		sn = getChildAtLevel(sn, 2);
+		if (sn.getID() == SLParserTreeConstants.JJTWFF) {
+			// Here we have an WFF
+			String expression = handleWff(sn);
+		    result.append("(defrule query-if ("+expression+") =>"+
+		    		      " (return Boolean ("+expression +")))");
 		}
 
 		return result.toString();
 	}
 
-	/**
-	 * Recursively walks through the tree of Parameters and translates them.
-	 * 
-	 * @param node
-	 *            The node to start from.
-	 * @return Either the bind if the node was a Fact or the value of the node
-	 *         if it was a Constant.
-	 */
-	public static String resolveParameters(SimpleNode node) {
-		// Go From Parameter to Node after Term
-		SimpleNode currNode = node;
-		while (currNode.getID() != SLParserTreeConstants.JJTTERM) {
-			currNode = getChild(currNode, 0);
-		}
-		currNode = getChild(currNode, 0);
-		if (currNode.getID() == SLParserTreeConstants.JJTFUNCTIONALTERM) {
-			// This would be a Fact for CLIPS
-
-			StringBuilder buffer = new StringBuilder(" ("
-					+ getChild(currNode, 0).getText() + " ");
-			for (int i = 1; i < currNode.jjtGetNumChildren(); ++i) {
-				SimpleNode child = getChild(currNode, i);
-				String childAssert = resolveParameters(child);
-				buffer.append(childAssert).append(")");
-			}
-
-			// System.out.println(buffer);
-			return buffer.toString();
-		} else if (currNode.getID() == SLParserTreeConstants.JJTCONSTANT) {
-			// Here we have simple constants
-
-			currNode = getChild(currNode, 0);
-			if (currNode.getID() == SLParserTreeConstants.JJTSTRING) {
-				// A String directly contains its value
-				return "\"" + currNode.getText() + "\"";
-			} else if (currNode.getID() == SLParserTreeConstants.JJTNUMERICALCONSTANT) {
-				// A numerical constant first has a node describing the type of
-				// the number (e.g. Integer). We don't check it here.
-				currNode = getChild(currNode, 0);
-				return currNode.getText();
-			}
-		} else if (currNode.getID() == SLParserTreeConstants.JJTSEQUENCE) {
-			// Here we have a MultiSlot in CLIPS
-
-			StringBuilder buffer = new StringBuilder();
-
-			for (int i = 0; i < currNode.jjtGetNumChildren(); ++i) {
-				String childBind = resolveParameters(getChild(currNode, i));
-				if (i > 0)
-					buffer.append(" ");
-				buffer.append(childBind);
-			}
-			return buffer.toString();
-		}
-		return "";
-	}
-
-	/**
-	 * Returns a child of SimpleNode directly as SimpleNode.
-	 * 
-	 * @param node
-	 *            The node whose child is of interest.
-	 * @param no
-	 *            The index of the child.
-	 * @return The child node.
-	 */
-	public static SimpleNode getChild(SimpleNode node, int no) {
-		return (SimpleNode) node.jjtGetChild(no);
-	}
-
-	/**
-	 * Returns the leftmost child at a certain level of the tree. Level means
-	 * here that we walk through the tree downwards beginning at
-	 * <code>node</code> for <code>level</code> levels.
-	 * 
-	 * @param node
-	 *            The node we start from.
-	 * @param level
-	 *            The level we walk through.
-	 * @return The child node at the specified level.
-	 */
-	public static SimpleNode getChildAtLevel(SimpleNode node, int level) {
-		for (int i = 0; i < level; ++i) {
-			node = getChild(node, 0);
-		}
-		return node;
-	}
-
-}
+}	
