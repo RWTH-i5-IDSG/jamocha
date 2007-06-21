afc28c8c796277dc9c733ecaa7c2b8a9cc095740
leloo <leloo@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-06-21 15:40:37 +0000

    adapted function descriptions added example
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1237 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/rete/functions/ruleengine/Exit.java
+++ src/main/org/jamocha/rete/functions/ruleengine/Exit.java
@@ -36,7 +36,7 @@ public class Exit implements Function, Serializable {
 	private static final class Description implements FunctionDescription {
 
 		public String getDescription() {
-			return "ExitFunction closes the engine and calls System.exit(0).";
+			return "Closes the engine.";
 		}
 
 		public int getParameterCount() {
@@ -68,8 +68,7 @@ public class Exit implements Function, Serializable {
 		}
 
 		public String getExample() {
-			// TODO Auto-generated method stub
-			return null;
+			return "(exit)";
 		}
 	}
 
