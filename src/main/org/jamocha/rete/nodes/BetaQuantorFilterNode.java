72e0f81f1be0533024a72c7d7ea7baf4968a31bd
procole <procole@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-07-06 13:49:14 +0000

    quantor nodes implemented but buggy
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1325 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/rete/nodes/BetaQuantorFilterNode.java
+++ src/main/org/jamocha/rete/nodes/BetaQuantorFilterNode.java
@@ -1,15 +1,49 @@
 package org.jamocha.rete.nodes;
 
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.jamocha.rete.Fact;
 import org.jamocha.rete.Rete;
 import org.jamocha.rete.exception.AssertException;
 import org.jamocha.rete.exception.RetractException;
+import org.jamocha.rete.nodes.joinfilter.JoinFilter;
+import org.jamocha.rete.nodes.joinfilter.JoinFilterException;
 
 public class BetaQuantorFilterNode extends BetaFilterNode {
 
-	public boolean negated;
+	class Marker<K> {
+		
+		protected Map<K,Boolean> map = null;
+		boolean def;
+		
+		public Marker(boolean def) {
+			map = new HashMap<K,Boolean>();
+			this.def=def;
+		}
+		
+		public void set(K v, boolean b){
+			map.put(v, b);
+		}
+		
+		public void mark(K v){
+			set(v, true);
+		}
+		
+		public void unmark(K v){
+			set(v, false);
+		}
+		
+		public boolean isMarked(K v){
+			Boolean res = map.get(v);
+			if (res == null) return def;
+			return res;
+		}
+	}
+	
+	protected boolean negated;
+	protected Marker<FactTuple> propagatedMarker = new Marker<FactTuple>(false);
 	
 	public BetaQuantorFilterNode(int id,boolean negated) {
 		super(id);
@@ -20,77 +54,142 @@ public class BetaQuantorFilterNode extends BetaFilterNode {
 		this(id,false);
 	}
 	
-	public boolean controlledNot(boolean control, boolean data){
-		if (control) {
-			return !data;
-		} else {
-			return data;
-		}
-		
+	protected void propagateNewTuple(FactTuple t, Rete e) throws AssertException {
+		FactTuple newTuple = t.addFact(e.getInitialFact());
+		propagatedMarker.mark(t);
+		mergeMemory.add(t);
+		propogateAssert(newTuple, e);
 	}
 
-	
-	@Override
-	protected boolean evaluate(FactTuple tuple, Fact rfcts) {
-		// TODO Auto-generated method stub
-		return false;
+	protected void unPropagateNewTuple(FactTuple t, Rete e) throws RetractException {
+		FactTuple newTuple = t.addFact(e.getInitialFact());
+		propagatedMarker.unmark(t);
+		mergeMemory.remove(t);
+		propogateRetract(newTuple, e);
 	}
 	
-	public void activate(Rete engine) throws AssertException {
-		if (!activated) {
-			// we have to traverse the whole beta mem and eval it.
-			activated = true;
-			if ( controlledNot(negated, alphaMemory.getSize()>0 ) ){
+	protected void evaluateBeta(FactTuple tuple, Rete engine) throws AssertException {
+		// iterate over alpha memory.
+		// check, whether there is a fact, which matches the filters.
+		// propagate them, depending on "negated" and the matching above and
+		// mark tuple as propagated somewhere
+		boolean thereIsAFact = false;
+		for (Fact alphaFact : alphaMemory) {
+			if ( evaluate(tuple, alphaFact) ) { thereIsAFact = true; break;}
+		}
+		boolean propagateIt = (thereIsAFact != negated);
+		if (propagateIt) {
+			propagateNewTuple(tuple, engine);
+		}
+	}
+
+	public void assertLeft(FactTuple tuple, Rete engine) throws AssertException {
+		betaMemory.add(tuple);
+		if (!activated) return;
+		
+		evaluateBeta(tuple, engine);
+	}
+
+	public void assertRight(Fact fact, Rete engine) throws AssertException {
+		
+		alphaMemory.add(fact);
+		if (!activated) return;
+		
+		// iterate over beta memory (for "negated" over the already propagated,
+		// else over the not propagated). check for each tuple, whether the
+		// propagation status must be changed and do it, if so.
+		for (FactTuple tuple : betaMemory){
+			if ( propagatedMarker.isMarked(tuple) != negated ) continue;
+			boolean matchesToNewAlpha = evaluate(tuple, fact);
+			if (!matchesToNewAlpha) continue;
+			if (negated  /* && propagated*/) {
 				try {
-					fire(engine);
+					unPropagateNewTuple(tuple, engine);
 				} catch (RetractException e) {
 					throw new AssertException(e);
 				}
+			} else /* if (!negated && !propagated )*/ {
+				propagateNewTuple(tuple, engine);
 			}
 		}
 	}
-	
-	private void fire(Rete engine) throws RetractException{
-		for (FactTuple t : betaMemory) {
-			FactTuple newTuple = t.addFact(engine.getFactById(0));
-			try {
-				propogateAssert(newTuple, engine);
-			} catch (AssertException e) {
-				throw new RetractException(e);
-			}
-			
-		}
+
+	public void retractLeft(FactTuple tuple, Rete engine) throws RetractException {
+		// simply retract it ;)
+		if (propagatedMarker.isMarked(tuple)) unPropagateNewTuple(tuple, engine);
+		betaMemory.remove(tuple);
 	}
-	
-	private void backfire(Rete engine) throws RetractException{
-		for (FactTuple t : betaMemory) {
-			this.propogateRetract(t, engine);
-		}
-	}
-	
-	
+
 	public void retractRight(Fact fact, Rete engine) throws RetractException {
-		alphaMemory.remove(fact);
-		if (alphaMemory.isEmpty() && negated) {
-			fire(engine);
-		} else if (alphaMemory.isEmpty() && !negated) {
-			backfire(engine);
-		}
-	}
-	
-	
-	public void assertRight(Fact fact, Rete engine) throws AssertException {
-		if (alphaMemory.isEmpty()) {
-			alphaMemory.add(fact);
-			if (activated) {
-				try{
-					if (negated) backfire(engine);	else fire(engine);
-				} catch (RetractException e){
-					throw new AssertException(e);
+		// retract it and check for all not propagated (or for "negated" all 
+		// propagated), whether this status must be changed. if so, do it.
+		for (FactTuple tuple: betaMemory) {
+			if (propagatedMarker.isMarked(tuple) != negated) continue;
+
+			boolean matchBeforeRetracting = false;
+			boolean matchAfterRetracting = false;
+			for (Fact alphaFact : alphaMemory) {
+				if ( evaluate(tuple, alphaFact) ) { 
+					matchBeforeRetracting = true;
+					if (alphaFact != fact ) matchAfterRetracting = true;
+				}
+				if (matchAfterRetracting && matchBeforeRetracting) break;			
+			}
+			if (matchBeforeRetracting && !matchAfterRetracting) {
+				if (negated) {
+					try {
+						propagateNewTuple(tuple, engine);
+					} catch (AssertException e) {
+						throw new RetractException(e);
+					}
+				} else {
+					unPropagateNewTuple(tuple, engine);					
 				}
 			}
-		} else {
-			alphaMemory.add(fact);			
+		}
+		alphaMemory.remove(fact);
+	}
+
+
+
+
+
+
+
+
+
+
+	public void activate(Rete engine) throws AssertException {
+		if (activated) return;
+		for (FactTuple tuple : betaMemory) {
+			evaluateBeta(tuple, engine);
 		}
 	}
+	
+
+	
+//	public void retractRight(Fact fact, Rete engine) throws RetractException {
+//		alphaMemory.remove(fact);
+//		if (alphaMemory.isEmpty() && negated) {
+//			fire(engine);
+//		} else if (alphaMemory.isEmpty() && !negated) {
+//			backfire(engine);
+//		}
+//	}
+	
+	
+//	public void assertRight(Fact fact, Rete engine) throws AssertException {
+//		if (alphaMemory.isEmpty()) {
+//			alphaMemory.add(fact);
+//			if (activated) {
+//				try{
+//					if (negated) backfire(engine);	else fire(engine);
+//				} catch (RetractException e){
+//					throw new AssertException(e);
+//				}
+//			}
+//		} else {
+//			alphaMemory.add(fact);			
+//		}
+//	}
 }
