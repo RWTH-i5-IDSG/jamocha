21eaaada2f4139df7ed185c6ea3d389e57801ffd
id864 <id864@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-08-06 10:28:43 +0000

    removed an @override, again. khk.
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1410 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/rete/agenda/FifoConflictResolutionStrategy.java
+++ src/main/org/jamocha/rete/agenda/FifoConflictResolutionStrategy.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class FifoConflictResolutionStrategy implements
 		ConflictResolutionStrategy {
 
-	@Override
 	public void addActivation(List<Activation> activations, Activation a) {
 		activations.add(a);
 	}
 
-	@Override
 	public void removeActivation(List<Activation> activations, Activation a) {
 		activations.remove(a);
 	}
