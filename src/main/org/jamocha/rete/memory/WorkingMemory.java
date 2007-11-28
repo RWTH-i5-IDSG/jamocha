d3e7d2cf3f2bf7b0293209068cc93b3cd4b085d5
procole <procole@2cd91b7d-3b21-0410-a5f4-f42db4f24360>
2007-11-28 11:51:03 +0000

    the whole infrastructure for the new memory is there, now i've to implement the data structure itself
    git-svn-id: https://jamocha.svn.sourceforge.net/svnroot/jamocha/jamocha@1887 2cd91b7d-3b21-0410-a5f4-f42db4f24360

--- src/main/org/jamocha/rete/memory/WorkingMemory.java
+++ src/main/org/jamocha/rete/memory/WorkingMemory.java
@@ -4,13 +4,15 @@ import org.jamocha.rete.nodes.BaseNode;
 
 public interface WorkingMemory {
 
-	public void addBeta(BaseNode owner);
+	public void addBeta(BaseNode owner, WorkingMemoryElement element);
 	
-	public void addAlpha(BaseNode owner);
+	public void addAlpha(BaseNode owner, WorkingMemoryElement element);
 	
 	public WmeIterator getWorkingMemoryElementIterator(BaseNode owner);
 	
-	public void removeFact(BaseNode owner);
+	public void removeAlpha(BaseNode owner, WorkingMemoryElement element);
+	
+	public void removeBeta(BaseNode owner, WorkingMemoryElement element);
 	
 	public void addWorkingMemoryListener(WorkingMemoryListener listener);
 	
--- src/main/org/jamocha/rete/memory/implementations/defaultimpl/WorkingMemoryImpl.java
+++ src/main/org/jamocha/rete/memory/implementations/defaultimpl/WorkingMemoryImpl.java
@@ -1,16 +1,16 @@
 package org.jamocha.rete.memory.implementations.defaultimpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jamocha.rete.memory.WmeIterator;
 import org.jamocha.rete.memory.WorkingMemory;
+import org.jamocha.rete.memory.WorkingMemoryElement;
 import org.jamocha.rete.memory.WorkingMemoryListener;
 import org.jamocha.rete.nodes.BaseNode;
 
 public class WorkingMemoryImpl implements WorkingMemory {
 
-	private WorkingMemoryImpl() {
-		
-	}
-	
 	private static WorkingMemoryImpl instance;
 	
 	public static WorkingMemoryImpl getWorkingMemory() {
@@ -19,6 +19,18 @@ public class WorkingMemoryImpl implements WorkingMemory {
 		}
 		return instance;
 	}
+	
+	
+
+	protected List<WorkingMemoryListener> listeners;
+	
+	
+	private WorkingMemoryImpl() {
+		listeners = new ArrayList<WorkingMemoryListener>();
+	}
+	
+	
+	
 
 	@Override
 	public WmeIterator getWorkingMemoryElementIterator(BaseNode owner) {
@@ -26,29 +38,53 @@ public class WorkingMemoryImpl implements WorkingMemory {
 		return null;
 	}
 
-	@Override
-	public void removeFact(BaseNode owner) {
-		// TODO Auto-generated method stub
-		
-	}
+
 	
 
-	@Override
-	public void addAlpha(BaseNode owner) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void addBeta(BaseNode owner) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	@Override
 	public void addWorkingMemoryListener(WorkingMemoryListener listener) {
-		// TODO Auto-generated method stub
+		listeners.add(listener);
+	}
+
+
+
+
+	@Override
+	public void addAlpha(BaseNode owner, WorkingMemoryElement element) {
 		
+
+		for (WorkingMemoryListener l : listeners) l.addedeToAlpha(element);
+	}
+
+
+
+
+	@Override
+	public void addBeta(BaseNode owner, WorkingMemoryElement element) {
+		
+		for (WorkingMemoryListener l : listeners) l.addedToBeta(element);
+	}
+
+
+
+
+	@Override
+	public void removeAlpha(BaseNode owner, WorkingMemoryElement element) {
+		
+
+		for (WorkingMemoryListener l : listeners) l.removedFromAlpha(element);
+	}
+
+
+
+
+	@Override
+	public void removeBeta(BaseNode owner, WorkingMemoryElement element) {
+	
+
+		for (WorkingMemoryListener l : listeners) l.removedFromBeta(element);
 	}
 
 }
