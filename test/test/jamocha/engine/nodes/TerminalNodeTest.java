9e82cc1e5b8a885f6f0fe38a816d44f83e149a89
Christoph Terwelp <christoph.terwelp@rwth-aachen.de>
2013-09-03 16:34:13 +0200

    Added TerminNodeTest

---
+++ test/test/jamocha/engine/nodes/TerminalNodeTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2002-2013 The Jamocha Team
+ * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.jamocha.org/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package test.jamocha.engine.nodes;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Christoph Terwelp <christoph.terwelp@rwth-aachen.de>
+ *
+ */
+public class TerminalNodeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.TerminalNode#newEdge(org.jamocha.dn.nodes.Node)}.
+	 */
+	@Test
+	public void testNewEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.TerminalNode#TerminalNode(org.jamocha.dn.Network)}.
+	 */
+	@Test
+	public void testTerminalNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#getChildren()}.
+	 */
+	@Test
+	public void testGetChildren() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#getMemory()}.
+	 */
+	@Test
+	public void testGetMemory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#numChildren()}.
+	 */
+	@Test
+	public void testNumChildren() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#delocalizeAddress(org.jamocha.dn.memory.FactAddress)}.
+	 */
+	@Test
+	public void testDelocalizeAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#enqueue(org.jamocha.dn.Token)}.
+	 */
+	@Test
+	public void testEnqueue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#getTokenQueue()}.
+	 */
+	@Test
+	public void testGetTokenQueue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.jamocha.dn.nodes.Node#getIncomingEdges()}.
+	 */
+	@Test
+	public void testGetIncomingEdges() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
