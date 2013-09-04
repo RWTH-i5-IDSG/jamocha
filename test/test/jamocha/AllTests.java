deeb509fbe138975468bd21602a50cacdfde6f2a
Kai Schwarz <kai.schwarz@rwth-aachen.de>
2013-09-04 08:03:43 +0000

    started documenting test.jamocha.*

--- test/test/jamocha/AllTests.java
+++ test/test/jamocha/AllTests.java
@@ -5,13 +5,17 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 /**
+ * Test suite for the whole jamocha project.
+ * 
+ * Add new package test suites or single tests to the SuiteClasses annotation.
+ * 
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-		test.jamocha.engine.memory.javaimpl.MemoryHandlerTempTest.class,
-		test.jamocha.engine.filter.AllFilterTests.class,
-		test.jamocha.engine.nodes.AllNodeTests.class })
+		test.jamocha.dn.memory.javaimpl.MemoryHandlerTempTest.class,
+		test.jamocha.filter.AllFilterTests.class,
+		test.jamocha.dn.nodes.AllNodeTests.class })
 public class AllTests {
 
 }
