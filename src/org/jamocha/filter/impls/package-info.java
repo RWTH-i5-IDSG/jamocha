1173b10f362f36da93d7f223d42853db9b266d8c
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2013-09-05 09:13:27 +0200

    package descriptions for filter/impls/*

--- src/org/jamocha/filter/impls/package-info.java
+++ src/org/jamocha/filter/impls/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains implementations of the {@link org.jamocha.filter.Function} interface.
+ * Contains classes loading the implementation of the {@link org.jamocha.filter.Function} interface.
  * 
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  * @see org.jamocha.filter.Function
