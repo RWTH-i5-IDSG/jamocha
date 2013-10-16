23ce917f46bb3a168d00a1c7f025059d9ad812e2
Kai Schwarz <kai.schwarz@rwth-aachen.de>
2013-10-16 14:00:53 +0000

    Moved FunctionBuilder and PredicateBuilder into seperate files

--- test/test/jamocha/dn/nodes/TokenProcessingTest.java
+++ test/test/jamocha/dn/nodes/TokenProcessingTest.java
@@ -14,29 +14,23 @@
  */
 package test.jamocha.dn.nodes;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import org.jamocha.dn.Network;
 import org.jamocha.dn.memory.SlotType;
 import org.jamocha.dn.memory.Template;
 import org.jamocha.dn.memory.javaimpl.SlotAddress;
-import org.jamocha.filter.ConstantLeaf;
 import org.jamocha.filter.Filter;
-import org.jamocha.filter.Function;
-import org.jamocha.filter.FunctionWithArguments;
-import org.jamocha.filter.FunctionWithArgumentsComposite;
 import org.jamocha.filter.Path;
-import org.jamocha.filter.PathLeaf;
 import org.jamocha.filter.Predicate;
 import org.jamocha.filter.PredicateWithArguments;
-import org.jamocha.filter.PredicateWithArgumentsComposite;
 import org.jamocha.filter.TODODatenkrakeFunktionen;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import test.jamocha.util.FunctionBuilder;
+import test.jamocha.util.PredicateBuilder;
 
 /**
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
@@ -74,112 +68,6 @@ public class TokenProcessingTest {
 
 	public static PredicateWithArguments combine() {
 		return null;
-	}
-
-	static class PredicateBuilder {
-		final Predicate predicate;
-		final List<FunctionWithArguments> args = new LinkedList<>();
-
-		public PredicateBuilder(final Predicate predicate) {
-			this.predicate = predicate;
-		}
-
-		public PredicateBuilder addPath(final Path path, final SlotAddress slot) {
-			final SlotType[] paramTypes = this.predicate.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != path.getTemplateSlotType(slot)) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(new PathLeaf(path, slot));
-			return this;
-		}
-
-		public PredicateBuilder addConstant(final Object value, final SlotType type) {
-			final SlotType[] paramTypes = this.predicate.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != type) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(new ConstantLeaf(value, type));
-			return this;
-		}
-
-		public PredicateBuilder addFunction(final FunctionWithArguments function) {
-			final SlotType[] paramTypes = this.predicate.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != function.getReturnType()) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(function);
-			return this;
-		}
-
-		public PredicateWithArguments build() {
-			if (this.predicate.getParamTypes().length != this.args.size()) {
-				throw new IllegalArgumentException("Wrong number of arguments!");
-			}
-			return new PredicateWithArgumentsComposite(this.predicate,
-					this.args.toArray(new FunctionWithArguments[this.args.size()]));
-		}
-	}
-
-	static class FunctionBuilder {
-		final Function<?> function;
-		final List<FunctionWithArguments> args = new LinkedList<>();
-
-		public FunctionBuilder(final Function<?> function) {
-			this.function = function;
-		}
-
-		public FunctionBuilder addPath(final Path path, final SlotAddress slot) {
-			final SlotType[] paramTypes = this.function.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != path.getTemplateSlotType(slot)) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(new PathLeaf(path, slot));
-			return this;
-		}
-
-		public FunctionBuilder addConstant(final Object value, final SlotType type) {
-			final SlotType[] paramTypes = this.function.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != type) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(new ConstantLeaf(value, type));
-			return this;
-		}
-
-		public FunctionBuilder addFunction(final FunctionWithArguments function) {
-			final SlotType[] paramTypes = this.function.getParamTypes();
-			if (paramTypes.length != this.args.size()) {
-				throw new IllegalArgumentException("All arguments already set!");
-			}
-			if (paramTypes[this.args.size() + 1] != function.getReturnType()) {
-				throw new IllegalArgumentException("Wrong argument type!");
-			}
-			this.args.add(function);
-			return this;
-		}
-
-		public FunctionWithArguments build() {
-			if (this.function.getParamTypes().length != this.args.size()) {
-				throw new IllegalArgumentException("Wrong number of arguments!");
-			}
-			return new FunctionWithArgumentsComposite(this.function,
-					this.args.toArray(new FunctionWithArguments[this.args.size()]));
-		}
 	}
 
 	/**
---
+++ test/test/jamocha/util/PredicateBuilder.java
@@ -0,0 +1,67 @@
+package test.jamocha.util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jamocha.dn.memory.SlotType;
+import org.jamocha.dn.memory.javaimpl.SlotAddress;
+import org.jamocha.filter.ConstantLeaf;
+import org.jamocha.filter.FunctionWithArguments;
+import org.jamocha.filter.Path;
+import org.jamocha.filter.PathLeaf;
+import org.jamocha.filter.Predicate;
+import org.jamocha.filter.PredicateWithArguments;
+import org.jamocha.filter.PredicateWithArgumentsComposite;
+
+public class PredicateBuilder {
+	final Predicate predicate;
+	final List<FunctionWithArguments> args = new LinkedList<>();
+
+	public PredicateBuilder(final Predicate predicate) {
+		this.predicate = predicate;
+	}
+
+	public PredicateBuilder addPath(final Path path, final SlotAddress slot) {
+		final SlotType[] paramTypes = this.predicate.getParamTypes();
+		if (paramTypes.length != this.args.size()) {
+			throw new IllegalArgumentException("All arguments already set!");
+		}
+		if (paramTypes[this.args.size() + 1] != path.getTemplateSlotType(slot)) {
+			throw new IllegalArgumentException("Wrong argument type!");
+		}
+		this.args.add(new PathLeaf(path, slot));
+		return this;
+	}
+
+	public PredicateBuilder addConstant(final Object value, final SlotType type) {
+		final SlotType[] paramTypes = this.predicate.getParamTypes();
+		if (paramTypes.length != this.args.size()) {
+			throw new IllegalArgumentException("All arguments already set!");
+		}
+		if (paramTypes[this.args.size() + 1] != type) {
+			throw new IllegalArgumentException("Wrong argument type!");
+		}
+		this.args.add(new ConstantLeaf(value, type));
+		return this;
+	}
+
+	public PredicateBuilder addFunction(final FunctionWithArguments function) {
+		final SlotType[] paramTypes = this.predicate.getParamTypes();
+		if (paramTypes.length != this.args.size()) {
+			throw new IllegalArgumentException("All arguments already set!");
+		}
+		if (paramTypes[this.args.size() + 1] != function.getReturnType()) {
+			throw new IllegalArgumentException("Wrong argument type!");
+		}
+		this.args.add(function);
+		return this;
+	}
+
+	public PredicateWithArguments build() {
+		if (this.predicate.getParamTypes().length != this.args.size()) {
+			throw new IllegalArgumentException("Wrong number of arguments!");
+		}
+		return new PredicateWithArgumentsComposite(this.predicate,
+				this.args.toArray(new FunctionWithArguments[this.args.size()]));
+	}
+}
