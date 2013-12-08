baaae66a87bf9a3e241162b8b652d5d073e6a326
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2013-12-08 22:11:54 +0100

    added n>m, n>=m, n<=m

---
+++ src/org/jamocha/filter/impls/predicates/LessOrEqual.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2002-2013 The Jamocha Team
+ * 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.jamocha.org/
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jamocha.filter.impls.predicates;
+
+import org.jamocha.dn.memory.SlotType;
+import org.jamocha.filter.Function;
+import org.jamocha.filter.Predicate;
+import org.jamocha.filter.FunctionDictionary;
+
+/**
+ * Implements the functionality of the binary less or equal ({@code <=}) operator.
+ * 
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
+ * @see Predicate
+ * @see FunctionDictionary
+ */
+public class LessOrEqual {
+
+	static {
+		FunctionDictionary.addImpl(new Predicate() {
+			@Override
+			public SlotType[] getParamTypes() {
+				return new SlotType[] { SlotType.LONG, SlotType.LONG };
+			}
+
+			@Override
+			public String toString() {
+				return "<=";
+			}
+
+			@Override
+			public Boolean evaluate(final Function<?>... params) {
+				return (Long) params[0].evaluate() <= (Long) params[1].evaluate();
+			}
+		});
+		FunctionDictionary.addImpl(new Predicate() {
+			@Override
+			public SlotType[] getParamTypes() {
+				return new SlotType[] { SlotType.DOUBLE, SlotType.DOUBLE };
+			}
+
+			@Override
+			public String toString() {
+				return "<=";
+			}
+
+			@Override
+			public Boolean evaluate(final Function<?>... params) {
+				return (Double) params[0].evaluate() <= (Double) params[1].evaluate();
+			}
+		});
+	}
+
+}
