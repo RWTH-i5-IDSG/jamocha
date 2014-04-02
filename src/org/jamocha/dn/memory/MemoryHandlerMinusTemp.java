c92791e50c9b794f05be367bf0f2d9e9b4b0ac88
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-04-02 14:17:42 +0200

    documentation

--- src/org/jamocha/dn/memory/MemoryHandlerMinusTemp.java
+++ src/org/jamocha/dn/memory/MemoryHandlerMinusTemp.java
@@ -1,4 +1,29 @@
+/*
+ * Copyright 2002-2013 The Jamocha Team
+ * 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.jamocha.org/
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.jamocha.dn.memory;
 
+import org.jamocha.dn.nodes.Edge;
+import org.jamocha.dn.nodes.Node;
+
+/**
+ * Interface for memories with short life time and negative annotation.
+ * 
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
+ * @see MemoryHandlerMain
+ * @see Node
+ * @see Edge
+ */
 public interface MemoryHandlerMinusTemp extends MemoryHandlerTemp {
 }
--- src/org/jamocha/dn/memory/MemoryHandlerTemp.java
+++ src/org/jamocha/dn/memory/MemoryHandlerTemp.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.jamocha.dn.nodes.Edge;
 
 /**
- * Base interface for memories with short life time.
+ * Base Interface for memories with short life time.
  * 
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  * @see Edge
  */
 public interface MemoryHandlerTemp extends MemoryHandler {
 	/**
-	 * Splits the temp memory into several memories each holding {@code size} entries (with the
-	 * possible exception of the last returned handler)
+	 * Splits the {@link MemoryHandlerTemp} into several memories each holding {@code size} entries
+	 * (with the possible exception of the last returned handler, which may contain less).
 	 * 
 	 * @param size
 	 *            number of entries to be held by each handler
@@ -21,6 +21,13 @@ public interface MemoryHandlerTemp extends MemoryHandler {
 	 */
 	public List<MemoryHandler> splitIntoChunksOfSize(final int size);
 
+	/**
+	 * Enqueues this {@link MemoryHandlerTemp} in the {@link Edge} passed. This is done by calling
+	 * the corresponding {@link Edge#enqueueMemory} method.
+	 * 
+	 * @param edge
+	 *            {@link Edge edge} the {@link MemoryHandlerTemp} is to be enqueued in.
+	 */
 	public void enqueueInEdge(final Edge edge);
 
 	/**
