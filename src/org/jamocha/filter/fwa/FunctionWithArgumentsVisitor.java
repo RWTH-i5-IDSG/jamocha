27b0f165096af1b6506563b57a6af45140002fef
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-04-17 12:15:40 +0200

    re-combined previously split interfaces

--- src/org/jamocha/filter/fwa/FunctionWithArgumentsVisitor.java
+++ src/org/jamocha/filter/fwa/FunctionWithArgumentsVisitor.java
@@ -1,11 +1,16 @@
 package org.jamocha.filter.fwa;
 
 import org.jamocha.filter.fwa.PathLeaf.ParameterLeaf;
+import org.jamocha.visitor.Visitor;
 
 /**
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  */
-public interface FunctionWithArgumentsVisitor extends FunctionWithArgumentsCompositeVisitor {
+public interface FunctionWithArgumentsVisitor extends Visitor {
+	public void visit(final FunctionWithArgumentsComposite functionWithArgumentsComposite);
+
+	public void visit(final PredicateWithArgumentsComposite predicateWithArgumentsComposite);
+
 	public void visit(final ConstantLeaf constantLeaf);
 
 	public void visit(final ParameterLeaf parameterLeaf);
