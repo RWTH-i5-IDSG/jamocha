2058ccff19125cc78ea32c69e66208141960eda4
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-05-15 16:25:34 +0200

    added DateTime parser in SlotType

--- src/org/jamocha/dn/memory/SlotType.java
+++ src/org/jamocha/dn/memory/SlotType.java
@@ -14,6 +14,10 @@
  */
 package org.jamocha.dn.memory;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
 /**
  * Enum holding all types this system supports.
  * 
@@ -35,7 +39,11 @@ public enum SlotType {
 	/**
 	 * Enum value for boolean types.
 	 */
-	BOOLEAN(Boolean.class);
+	BOOLEAN(Boolean.class),
+	/**
+	 * Enum value for date time types.
+	 */
+	DATETIME(ZonedDateTime.class);
 
 	final private Class<?> javaClass;
 
@@ -56,5 +64,28 @@ public enum SlotType {
 	@Override
 	public String toString() {
 		return this.name();
+	}
+
+	public static ZonedDateTime convert(final String image) {
+		// < #GMT_OFFSET: ("+"|"-") <DIGIT> <DIGIT> >
+		// < #DATE: <DIGIT> <DIGIT> <DIGIT> <DIGIT> "-" <DIGIT> <DIGIT> "-" <DIGIT> <DIGIT> >
+		// < #TIME: <DIGIT> <DIGIT> ":" <DIGIT> <DIGIT> ( ":" <DIGIT> <DIGIT>)? >
+		// < DATETIME: <DATE> ( " " <TIME> (<GMT_OFFSET>)? )? >
+		return ZonedDateTime.parse(
+				image,
+				new DateTimeFormatterBuilder()
+						// date
+						.appendValue(ChronoField.YEAR, 4)
+						.appendLiteral('-')
+						.appendValue(ChronoField.MONTH_OF_YEAR, 2)
+						.appendLiteral('-')
+						.appendValue(ChronoField.DAY_OF_MONTH, 2)
+						// optional time
+						.optionalStart().appendLiteral(' ').appendValue(ChronoField.HOUR_OF_DAY, 2)
+						.appendLiteral(':').appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+						.optionalStart().appendLiteral(':')
+						.appendValue(ChronoField.SECOND_OF_MINUTE, 2).optionalEnd().optionalEnd()
+						// optional offset
+						.optionalStart().appendOffset("+HH", "+HH").optionalEnd().toFormatter());
 	}
 }
