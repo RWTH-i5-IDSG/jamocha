b3f6c5e8bd1ebda246352952932a8e1c57f3c383
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-05-16 14:34:27 +0200

    new SFP, first try

---
+++ src/org/jamocha/languages/common/beans/SlotDefinitionBean.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2002-2014 The Jamocha Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * 
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.jamocha.org/
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jamocha.languages.common.beans;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+import org.jamocha.languages.common.BeansVisitor;
+import org.jamocha.languages.common.ScopeStack.SymbolBean;
+import org.jamocha.visitor.Visitable;
+
+/**
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
+ *
+ */
+@Data
+public class SlotDefinitionBean implements Visitable<BeansVisitor> {
+	SymbolBean name;
+	final ArrayList<TemplateAttributeBean> templateAttributes = new ArrayList<>();
+
+	public void addTemplateAttribute(final TemplateAttributeBean templateAttribute) {
+		this.templateAttributes.add(templateAttribute);
+	}
+
+	@Override
+	public <T extends BeansVisitor> T accept(final T visitor) {
+		visitor.visit(this);
+		return visitor;
+	}
+}
