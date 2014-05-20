5675f0e36e31d6d627ca3c6cb78cb47c55954a40
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-05-20 17:51:32 +0200

    implemented Template construction in visitor for JJTree AST

--- src/org/jamocha/languages/clips/parser/SFPVisitorImpl.java
+++ src/org/jamocha/languages/clips/parser/SFPVisitorImpl.java
@@ -15,23 +15,43 @@
 package org.jamocha.languages.clips.parser;
 
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.LinkedList;
 
-import lombok.RequiredArgsConstructor;
-
+import org.jamocha.dn.memory.SlotType;
+import org.jamocha.dn.memory.Template;
+import org.jamocha.dn.memory.Template.Slot;
+import org.jamocha.filter.Function;
 import org.jamocha.languages.clips.parser.generated.Node;
+import org.jamocha.languages.clips.parser.generated.SFPBooleanType;
+import org.jamocha.languages.clips.parser.generated.SFPConstructDescription;
+import org.jamocha.languages.clips.parser.generated.SFPDateTimeType;
+import org.jamocha.languages.clips.parser.generated.SFPDeftemplateConstruct;
 import org.jamocha.languages.clips.parser.generated.SFPFalse;
 import org.jamocha.languages.clips.parser.generated.SFPFloat;
+import org.jamocha.languages.clips.parser.generated.SFPFloatType;
 import org.jamocha.languages.clips.parser.generated.SFPInteger;
+import org.jamocha.languages.clips.parser.generated.SFPIntegerType;
+import org.jamocha.languages.clips.parser.generated.SFPParser;
+import org.jamocha.languages.clips.parser.generated.SFPSingleSlotDefinition;
+import org.jamocha.languages.clips.parser.generated.SFPSlotDefinition;
 import org.jamocha.languages.clips.parser.generated.SFPStart;
 import org.jamocha.languages.clips.parser.generated.SFPString;
+import org.jamocha.languages.clips.parser.generated.SFPStringType;
 import org.jamocha.languages.clips.parser.generated.SFPSymbol;
+import org.jamocha.languages.clips.parser.generated.SFPSymbolType;
 import org.jamocha.languages.clips.parser.generated.SFPTrue;
+import org.jamocha.languages.clips.parser.generated.SFPTypeAttribute;
+import org.jamocha.languages.clips.parser.generated.SFPTypeSpecification;
+import org.jamocha.languages.clips.parser.generated.SimpleNode;
 import org.jamocha.languages.common.ScopeStack;
 import org.jamocha.languages.common.ScopeStack.Symbol;
 
 public class SFPVisitorImpl implements SelectiveSFPVisitor {
 
 	final ScopeStack scope = new ScopeStack();
+	final HashMap<Symbol, Template> symbolTableTemplates = new HashMap<>();
+	final HashMap<Symbol, Function<?>> symbolTableFunctions = new HashMap<>();
 
 	@Override
 	public Object visit(SFPStart node, Object data) {
@@ -39,13 +59,8 @@ public class SFPVisitorImpl implements SelectiveSFPVisitor {
 	}
 
 	@Override
-	public Object visit(SFPFloat node, Object data) {
-		return Double.parseDouble(node.jjtGetValue().toString());
-	}
-
-	@Override
-	public Object visit(SFPInteger node, Object data) {
-		return Long.parseLong(node.jjtGetValue().toString());
+	public Object visit(SFPDeftemplateConstruct node, Object data) {
+		return sendVisitor(new SFPDeftemplateConstructVisitor(), node, data).template;
 	}
 
 	public <V extends SelectiveSFPVisitor, N extends Node> V sendVisitor(final V visitor,
@@ -54,14 +69,9 @@ public class SFPVisitorImpl implements SelectiveSFPVisitor {
 		return visitor;
 	}
 
-	@RequiredArgsConstructor
-	static enum Type {
-		Nil, DateTime, Symbol, String, Integer, Float, BooleanSymbol;
-
-		final static EnumSet<Type> Number = EnumSet.of(Integer, Float);
-		final static EnumSet<Type> Constant = EnumSet.of(Nil, DateTime, Symbol, String, Integer,
-				Float, BooleanSymbol);
-	}
+	final static EnumSet<SlotType> Number = EnumSet.of(SlotType.LONG, SlotType.DOUBLE);
+	final static EnumSet<SlotType> Constant = EnumSet.of(SlotType.NIL, SlotType.DATETIME,
+			SlotType.SYMBOL, SlotType.STRING, SlotType.LONG, SlotType.DOUBLE, SlotType.BOOLEAN);
 
 	class SFPSymbolVisitor implements SelectiveSFPVisitor {
 		Symbol symbol;
@@ -82,78 +92,292 @@ public class SFPVisitorImpl implements SelectiveSFPVisitor {
 	}
 
 	class SFPValueVisitor implements SelectiveSFPVisitor {
-		Type type;
+		SlotType type;
 		Object value;
-		final EnumSet<Type> allowed;
+		final EnumSet<SlotType> allowed;
 
-		public SFPValueVisitor(final Type firstAllowed, final Type... restAllowed) {
+		public SFPValueVisitor(final SlotType firstAllowed, final SlotType... restAllowed) {
 			this.allowed = EnumSet.of(firstAllowed, restAllowed);
 		}
 
-		public SFPValueVisitor(final Type allowed) {
+		public SFPValueVisitor(final SlotType allowed) {
 			this.allowed = EnumSet.of(allowed);
 		}
 
-		public SFPValueVisitor(final EnumSet<Type> allowed) {
+		public SFPValueVisitor(final EnumSet<SlotType> allowed) {
 			this.allowed = allowed;
 		}
 
 		public SFPValueVisitor() {
-			this.allowed = EnumSet.allOf(Type.class);
+			this.allowed = EnumSet.allOf(SlotType.class);
 		}
 
 		@Override
 		public Object visit(SFPFloat node, Object data) {
-			if (!allowed.contains(Type.Float))
+			if (!allowed.contains(SlotType.DOUBLE))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.Float;
+			this.type = SlotType.DOUBLE;
 			this.value = Double.parseDouble(node.jjtGetValue().toString());
 			return data;
 		}
 
 		@Override
 		public Object visit(SFPInteger node, Object data) {
-			if (!allowed.contains(Type.Integer))
+			if (!allowed.contains(SlotType.LONG))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.Integer;
+			this.type = SlotType.LONG;
 			this.value = Long.parseLong(node.jjtGetValue().toString());
 			return data;
 		}
 
 		@Override
 		public Object visit(SFPSymbol node, Object data) {
-			if (!allowed.contains(Type.Symbol))
+			if (!allowed.contains(SlotType.SYMBOL))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.Symbol;
+			this.type = SlotType.SYMBOL;
 			this.value = sendVisitor(new SFPSymbolVisitor(), node, data).symbol;
 			return data;
 		}
 
 		@Override
 		public Object visit(SFPTrue node, Object data) {
-			if (!allowed.contains(Type.BooleanSymbol))
+			if (!allowed.contains(SlotType.BOOLEAN))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.BooleanSymbol;
+			this.type = SlotType.BOOLEAN;
 			this.value = true;
 			return data;
 		}
 
 		@Override
 		public Object visit(SFPFalse node, Object data) {
-			if (!allowed.contains(Type.BooleanSymbol))
+			if (!allowed.contains(SlotType.BOOLEAN))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.BooleanSymbol;
+			this.type = SlotType.BOOLEAN;
 			this.value = false;
 			return data;
 		}
 
 		@Override
 		public Object visit(SFPString node, Object data) {
-			if (!allowed.contains(Type.String))
+			if (!allowed.contains(SlotType.STRING))
 				return SFPVisitorImpl.this.visit(node, data);
-			this.type = Type.String;
+			this.type = SlotType.STRING;
 			this.value = sendVisitor(new SFPStringVisitor(), node, data).string;
 			return data;
+		}
+		// TBD: Nil, DateTime
+	}
+
+	class SFPTypeVisitor implements SelectiveSFPVisitor {
+		SlotType type;
+		final EnumSet<SlotType> allowed;
+
+		public SFPTypeVisitor(final SlotType firstAllowed, final SlotType... restAllowed) {
+			this.allowed = EnumSet.of(firstAllowed, restAllowed);
+		}
+
+		public SFPTypeVisitor(final SlotType allowed) {
+			this.allowed = EnumSet.of(allowed);
+		}
+
+		public SFPTypeVisitor(final EnumSet<SlotType> allowed) {
+			this.allowed = allowed;
+		}
+
+		public SFPTypeVisitor() {
+			this.allowed = EnumSet.allOf(SlotType.class);
+		}
+
+		@Override
+		public Object visit(SFPFloatType node, Object data) {
+			if (!allowed.contains(SlotType.DOUBLE))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.DOUBLE;
+			return data;
+		}
+
+		@Override
+		public Object visit(SFPIntegerType node, Object data) {
+			if (!allowed.contains(SlotType.LONG))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.LONG;
+			return data;
+		}
+
+		@Override
+		public Object visit(SFPSymbolType node, Object data) {
+			if (!allowed.contains(SlotType.SYMBOL))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.SYMBOL;
+			return data;
+		}
+
+		@Override
+		public Object visit(SFPBooleanType node, Object data) {
+			if (!allowed.contains(SlotType.BOOLEAN))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.BOOLEAN;
+			return data;
+		}
+
+		@Override
+		public Object visit(SFPStringType node, Object data) {
+			if (!allowed.contains(SlotType.STRING))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.STRING;
+			return data;
+		}
+
+		@Override
+		public Object visit(SFPDateTimeType node, Object data) {
+			if (!allowed.contains(SlotType.DATETIME))
+				return SFPVisitorImpl.this.visit(node, data);
+			this.type = SlotType.DATETIME;
+			return data;
+		}
+		// TBD: LEXEME = STRING | SYMBOL, NUMBER = INTEGER | FLOAT
+	}
+
+	class SFPTypeSpecificationVisitor implements SelectiveSFPVisitor {
+		SlotType type;
+
+		// <type-specification> ::= <allowed-type>+ | ?VARIABLE
+		// void: ( ( AllowedType() )+ | VariableType() )
+
+		// <allowed-type> ::= SYMBOL | STRING | LEXEME | INTEGER | FLOAT | NUMBER |
+		// EXTERNAL-ADDRESS | FACT-ADDRESS | DATETIME
+		// void: ( SymbolType() | StringType() | DateTimeType() | LexemeType() | IntegerType() |
+		// FloatType() | NumberType() | BooleanType() )
+
+		@Override
+		public Object visit(SFPTypeSpecification node, Object data) {
+			if (node.jjtGetNumChildren() != 1)
+				throw new IllegalArgumentException(
+						"Restriction of template fields to multiple types is not supported at the moment!");
+			assert node.jjtGetNumChildren() == 1;
+			// TBD: LEXEME, NUMBER
+			this.type =
+					sendVisitor(
+							new SFPTypeVisitor(EnumSet.of(/* SlotType.LEXEME, */SlotType.SYMBOL,
+									SlotType.STRING, SlotType.DATETIME, SlotType.LONG,
+									SlotType.DOUBLE/* , SlotType.NUMBER */)), node.jjtGetChild(0),
+							data).type;
+			return data;
+		}
+		// TBD: VariableType
+	}
+
+	class SFPTemplateAttributeVisitor implements SelectiveSFPVisitor {
+		SlotType slotType;
+
+		// <template-attribute> ::= <default-attribute> | <constraint-attribute>
+		// <LBRACE> ( DefaultAttribute() | DynamicAttribute() | ConstraintAttribute() ) <RBRACE>
+
+		// <constraint-attribute> ::= <type-attribute> | <allowed-constant-attribute> |
+		// <range-attribute> | <cardinality-attribute>
+		// ( TypeAttribute() | AllowedConstantAttribute() | RangeAttribute() |
+		// CardinalityAttribute() )
+
+		// <type-attribute> ::= (type <type-specification>)
+		// <TYPE> TypeSpecification()
+		@Override
+		public Object visit(SFPTypeAttribute node, Object data) {
+			assert node.jjtGetNumChildren() == 1;
+			this.slotType =
+					sendVisitor(new SFPTypeSpecificationVisitor(), node.jjtGetChild(0), data).type;
+			return data;
+		}
+	}
+
+	class SFPSingleSlotDefinitionVisitor implements SelectiveSFPVisitor {
+		Slot slot;
+
+		// <single-slot-definition> ::= ( slot <slot-name> <template-attribute>*)
+		// <SLOT> ( Symbol() ( TemplateAttribute() )* )
+		@Override
+		public Object visit(SFPSingleSlotDefinition node, Object data) {
+			assert node.jjtGetNumChildren() > 0;
+			if (node.jjtGetNumChildren() != 2)
+				throw new IllegalArgumentException(
+						"For now, slot definitions consist of a name and a type restriction!");
+			final Symbol name =
+					sendVisitor(new SFPSymbolVisitor(), node.jjtGetChild(0), data).symbol;
+			final SlotType type =
+					sendVisitor(new SFPTemplateAttributeVisitor(), node.jjtGetChild(1), data).slotType;
+			this.slot = new Slot(type, name.getImage());
+			return data;
+		}
+	}
+
+	class SFPConstructDescriptionAndSlotDefinitionVisitor implements SelectiveSFPVisitor {
+		String comment;
+		final LinkedList<Slot> slotDefinitions = new LinkedList<>();
+
+		// <comment> ::= <string>
+		public Object visit(SFPConstructDescription node, Object data) {
+			assert node.jjtGetNumChildren() == 1;
+			this.comment = sendVisitor(new SFPStringVisitor(), node.jjtGetChild(0), data).string;
+			return data;
+		};
+
+		// <slot-definition> ::= <single-slot-definition> | <multislot-definition>
+		public Object visit(SFPSlotDefinition node, Object data) {
+			assert node.jjtGetNumChildren() == 1;
+			// TBD add support for multislot-definition
+			this.slotDefinitions.add(sendVisitor(new SFPSingleSlotDefinitionVisitor(),
+					node.jjtGetChild(0), data).slot);
+			return data;
+		};
+	}
+
+	class SFPDeftemplateConstructVisitor implements SelectiveSFPVisitor {
+		Template template;
+
+		// <comment> ::= <string>
+
+		// <deftemplate-construct> ::= (deftemplate <deftemplate-name> [<comment>]
+		// <slot-definition>*)
+
+		// <DEFTEMPLATE> Symbol() [ ConstructDescription() ] ( SlotDefinition() )*
+		public Object visit(SFPDeftemplateConstruct node, Object data) {
+			assert this.template == null;
+			assert node.jjtGetNumChildren() > 0;
+			final Symbol symbol =
+					sendVisitor(new SFPSymbolVisitor(), node.jjtGetChild(0), data).symbol;
+			final SFPConstructDescriptionAndSlotDefinitionVisitor visitor =
+					new SFPConstructDescriptionAndSlotDefinitionVisitor();
+			for (int i = 1; i < node.jjtGetNumChildren(); ++i) {
+				node.jjtGetChild(i).jjtAccept(visitor, data);
+			}
+			final String comment = visitor.comment;
+			this.template =
+					new Template(visitor.slotDefinitions.toArray(new Slot[visitor.slotDefinitions
+							.size()]));
+			SFPVisitorImpl.this.symbolTableTemplates.put(symbol, this.template);
+			return data;
+		};
+	}
+
+	public static void main(String[] args) {
+		boolean verbose = (args != null && args.length == 1 && "verbose".equals(args[0]));
+		if (!verbose)
+			System.out
+					.println("Note: For verbose output type \u005c"java Main verbose\u005c".\u005cn");
+		System.out.print("SFP> ");
+		SFPParser p = new SFPParser(System.in);
+		try {
+			while (true) {
+				SFPStart n = p.Start();
+				if (n == null)
+					System.exit(0);
+				n.dump(" ");
+				Object a = n.jjtAccept(new SFPVisitorImpl(), "");
+				System.out.println(a);
+			}
+		} catch (Exception e) {
+			System.err.println("ERROR[" + e.getClass().getSimpleName() + "]: " + e.getMessage());
+			if (verbose)
+				e.printStackTrace();
 		}
 	}
 }
