ef300dc93088c17cc3f473a70f504ba32ac200ae
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-07-03 13:55:44 +0200

    added type-safe toArray helper method

---
+++ src/org/jamocha/util/ToArray.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2002-2014 The Jamocha Team
+ * 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.jamocha.org/
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jamocha.util;
+
+import java.util.Collection;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+/**
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
+ */
+public class ToArray {
+	public static <T> T[] toArray(Collection<T> list, IntFunction<T[]> gen) {
+		return toArray(list.stream(), gen);
+	}
+
+	public static <T> T[] toArray(Stream<T> stream, IntFunction<T[]> gen) {
+		return stream.toArray(gen);
+	}
+}
