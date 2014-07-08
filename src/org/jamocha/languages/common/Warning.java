8350592996bf37b336b3c359ef40f4e6bab4bd18
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-07-08 14:50:18 +0200

    added error and warning classes

---
+++ src/org/jamocha/languages/common/Warning.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2002-2014 The Jamocha Team
+ * 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.jamocha.org/
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jamocha.languages.common;
+
+import lombok.Value;
+
+/**
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
+ */
+@Value
+public class Warning {
+	final String message;
+
+	@Override
+	public String toString() {
+		return "Warning: " + message;
+	}
+}
