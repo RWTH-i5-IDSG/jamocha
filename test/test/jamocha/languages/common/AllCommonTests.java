12869371cb3432f4b3f8742762b11791e9e3a463
Christoph Terwelp <christoph.terwelp@rwth-aachen.de>
2014-08-19 13:21:33 +0200

    Fixed AllCommonTests TestSuite.

--- test/test/jamocha/languages/common/AllCommonTests.java
+++ test/test/jamocha/languages/common/AllCommonTests.java
@@ -14,7 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(Suite.class)
-@SuiteClasses({ ConditionalElementTest.class })
+@SuiteClasses({ RuleConditionProcessorTest.class })
 public class AllCommonTests {
 
 }
