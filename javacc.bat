a8fcb519849d2a4c60f2d2c1e783b5e33fd274ef
Christoph Terwelp <christoph.terwelp@rwth-aachen.de>
2014-08-19 15:29:33 +0200

    Removed javacc scripts.

--- javacc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set javacc=java -cp c:\\dev\\libs\\javacc\\bin\\javacc.jar
-%javacc% jjtree src/org/jamocha/languages/clips/parser/SFP.jjt
-%javacc% javacc generated/org/jamocha/languages/clips/parser/generated/SFP.jj
-pause
--- javacc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-jjtree src/org/jamocha/languages/clips/parser/SFP.jjt
-javacc generated/org/jamocha/languages/clips/parser/generated/SFP.jj
-
