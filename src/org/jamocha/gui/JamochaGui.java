1ab401e84b018caa2d23580f742c68d428a466b1
Christoph Terwelp <christoph.terwelp@rwth-aachen.de>
2014-10-22 14:52:08 +0200

    minor changes in gui.

--- src/org/jamocha/gui/JamochaGui.java
+++ src/org/jamocha/gui/JamochaGui.java
@@ -44,64 +44,66 @@ import org.jamocha.languages.clips.parser.generated.ParseException;
  */
 @SuppressWarnings("restriction")
 public class JamochaGui extends Application {
-	
+
 	static File file = null;
 	private TextArea log;
 	private Stage primaryStage = null;
 	private Jamocha jamocha;
-	
+
 	private Scene generateScene() {
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		
+
 		Text scenetitle = new Text("Welcome to Jamocha");
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		GridPane.setMargin(scenetitle, new Insets(0, 25, 0, 0));
 		grid.add(scenetitle, 0, 1);
-		
+
 		log = new TextArea();
 		log.setEditable(false);
-		
+
 		grid.add(log, 1, 0, 1, 2);
 		ColumnConstraints columnConstraints = new ColumnConstraints();
-        columnConstraints.setFillWidth(true);
-        columnConstraints.setHgrow(Priority.ALWAYS);
-        grid.getColumnConstraints().add(columnConstraints);
-        
-        RowConstraints rowConstraints = new RowConstraints();
-        rowConstraints.setFillHeight(true);
-        rowConstraints.setVgrow(Priority.ALWAYS);
-        grid.getRowConstraints().add(rowConstraints);
-        
+		columnConstraints.setFillWidth(true);
+		columnConstraints.setHgrow(Priority.ALWAYS);
+		grid.getColumnConstraints().addAll(new ColumnConstraints(), columnConstraints);
+
+		RowConstraints rowConstraints = new RowConstraints();
+		rowConstraints.setFillHeight(true);
+		rowConstraints.setVgrow(Priority.ALWAYS);
+		grid.getRowConstraints().add(rowConstraints);
+
 		Scene scene = new Scene(grid);
 		grid.prefHeightProperty().bind(scene.heightProperty());
 		grid.prefWidthProperty().bind(scene.widthProperty());
 		return scene;
 	}
-	
+
 	private void log(String text) {
 		log.appendText(text + "\n");
 	}
-	
+
 	private void loadState(Stage primaryStage) {
-	    Preferences userPrefs = Preferences.userNodeForPackage(getClass());
-	    // get window location from user preferences: use x=100, y=100, width=400, height=400 as default
-	    primaryStage.setX(userPrefs.getDouble("stage.x", 100));
-	    primaryStage.setY(userPrefs.getDouble("stage.y", 100));
-	    primaryStage.setWidth(userPrefs.getDouble("stage.width", 800));
-	    primaryStage.setHeight(userPrefs.getDouble("stage.height", 600));
+		Preferences userPrefs = Preferences.userNodeForPackage(getClass());
+		// get window location from user preferences: use x=100, y=100, width=400, height=400 as
+		// default
+		primaryStage.setX(userPrefs.getDouble("stage.x", 100));
+		primaryStage.setY(userPrefs.getDouble("stage.y", 100));
+		primaryStage.setWidth(userPrefs.getDouble("stage.width", 800));
+		primaryStage.setHeight(userPrefs.getDouble("stage.height", 600));
 	}
-	
+
 	private void saveState(Stage primaryStage) {
 		Preferences userPrefs = Preferences.userNodeForPackage(getClass());
-	    userPrefs.putDouble("stage.x", primaryStage.getX());
-	    userPrefs.putDouble("stage.y", primaryStage.getY());
-	    userPrefs.putDouble("stage.width", primaryStage.getWidth());
-	    userPrefs.putDouble("stage.height", primaryStage.getHeight());
+		userPrefs.putDouble("stage.x", primaryStage.getX());
+		userPrefs.putDouble("stage.y", primaryStage.getY());
+		userPrefs.putDouble("stage.width", primaryStage.getWidth());
+		userPrefs.putDouble("stage.height", primaryStage.getHeight());
 	}
-	
+
 	private void loadFile(File file) {
 		try {
 			FileInputStream fileInputStream = new FileInputStream(file);
@@ -118,25 +120,25 @@ public class JamochaGui extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		
+
 		jamocha = new Jamocha();
-		
+
 		Scene scene = generateScene();
-		
+
 		if (file == null) {
 			FileChooser fileChooser = new FileChooser();
 			ExtensionFilter filter = new ExtensionFilter("CLIPS files", "*.clips");
 			fileChooser.getExtensionFilters().add(filter);
 			file = fileChooser.showOpenDialog(primaryStage);
 		}
-		
+
 		if (file != null) {
 			log("Opening file: \"" + file.getName() + "\"");
 			loadFile(file);
 		} else {
 			log("No file selected!");
 		}
-		
+
 		primaryStage.setMinWidth(800);
 		primaryStage.setMinHeight(600);
 		primaryStage.setTitle("Jamocha");
@@ -148,6 +150,7 @@ public class JamochaGui extends Application {
 	@Override
 	public void stop() {
 		saveState(primaryStage);
+		jamocha.shutdown();
 	}
 
 	public static void main(String[] args) {
