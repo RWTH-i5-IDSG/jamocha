95bf824be72933fca914de5088c3ff2323c52ee8
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2014-11-28 15:44:24 +0100

    Equals for all (homogeneous) kinds of arguments

--- src/org/jamocha/function/impls/predicates/Equals.java
+++ src/org/jamocha/function/impls/predicates/Equals.java
@@ -15,6 +15,8 @@
 
 package org.jamocha.function.impls.predicates;
 
+import java.util.Objects;
+
 import org.jamocha.dn.memory.SlotType;
 import org.jamocha.function.CommutativeFunction;
 import org.jamocha.function.Function;
@@ -45,69 +47,40 @@ public abstract class Equals extends Predicate implements CommutativeFunction<Bo
 	}
 
 	static {
-		FunctionDictionary.addImpl(new Equals() {
-			@Override
-			public SlotType[] getParamTypes() {
-				return new SlotType[] { SlotType.LONG, SlotType.LONG };
-			}
+		for (final SlotType slotType : SlotType.values()) {
+			FunctionDictionary.addImpl(new Equals() {
+				final SlotType[] types = SlotType.nCopies(slotType, 2);
 
-			@Override
-			public Boolean evaluate(final Function<?>... params) {
-				return ((Long) params[0].evaluate()).equals(params[1].evaluate());
-			}
-		});
-		FunctionDictionary.addImpl(new Equals() {
-			@Override
-			public SlotType[] getParamTypes() {
-				return new SlotType[] { SlotType.DOUBLE, SlotType.DOUBLE };
-			}
-
-			@Override
-			public Boolean evaluate(final Function<?>... params) {
-				return ((Double) params[0].evaluate()).equals(params[1].evaluate());
-			}
-		});
-		FunctionDictionary.addImpl(new Equals() {
-			@Override
-			public SlotType[] getParamTypes() {
-				return new SlotType[] { SlotType.BOOLEAN, SlotType.BOOLEAN };
-			}
-
-			@Override
-			public Boolean evaluate(final Function<?>... params) {
-				return ((Boolean) params[0].evaluate()).equals(params[1].evaluate());
-			}
-		});
-		FunctionDictionary.addImpl(new Equals() {
-			@Override
-			public SlotType[] getParamTypes() {
-				return new SlotType[] { SlotType.STRING, SlotType.STRING };
-			}
-
-			@Override
-			public Boolean evaluate(final Function<?>... params) {
-				return ((String) params[0].evaluate()).equals(params[1].evaluate());
-			}
-		});
-		FunctionDictionary.addGenerator(inClips, SlotType.BOOLEAN, (final SlotType[] paramTypes) -> {
-			return new Equals() {
 				@Override
 				public SlotType[] getParamTypes() {
-					return paramTypes;
+					return types;
 				}
 
 				@Override
 				public Boolean evaluate(final Function<?>... params) {
-					final boolean value = (Boolean) params[0].evaluate();
-					for (int i = 1; i < params.length; i++) {
-						final Function<?> param = params[i];
-						if (value != (Boolean) param.evaluate()) {
-							return Boolean.FALSE;
-						}
-					}
-					return Boolean.TRUE;
+					return Objects.equals(params[0].evaluate(), params[1].evaluate());
 				}
-			};
-		});
+			});
+			FunctionDictionary.addGenerator(inClips, slotType, (final SlotType[] paramTypes) -> {
+				return new Equals() {
+					@Override
+					public SlotType[] getParamTypes() {
+						return paramTypes;
+					}
+
+					@Override
+					public Boolean evaluate(final Function<?>... params) {
+						final Object value = params[0].evaluate();
+						for (int i = 1; i < params.length; i++) {
+							final Function<?> param = params[i];
+							if (!Objects.equals(value, param.evaluate())) {
+								return Boolean.FALSE;
+							}
+						}
+						return Boolean.TRUE;
+					}
+				};
+			});
+		}
 	}
 }
