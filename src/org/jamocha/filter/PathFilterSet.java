52095f7cace70c030958319e113629118affc94d
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2015-07-12 13:42:38 +0200

    reduced PathFilterSet::existentialClosure to a single PathFilter

--- src/org/jamocha/filter/PathFilterSet.java
+++ src/org/jamocha/filter/PathFilterSet.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2015 The Jamocha Team
- *
- *
+ * 
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.jamocha.org/
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -36,7 +36,7 @@ public interface PathFilterSet extends Visitable<PathFilterSetVisitor> {
 		final Path initialPath;
 		final Set<Path> existentialPaths;
 		final Set<PathFilterSet> purePart;
-		final Set<PathFilterSet> existentialClosure;
+		final PathFilter existentialClosure;
 
 		@Override
 		public <V extends PathFilterSetVisitor> V accept(final V visitor) {
