1915f4ba730b94494d489b8fce9cc8472186cc78
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2015-09-19 16:23:43 +0200

    FilterFunctionCompare::equals respects joinedWith

--- src/org/jamocha/filter/FilterFunctionCompare.java
+++ src/org/jamocha/filter/FilterFunctionCompare.java
@@ -566,7 +566,9 @@ public abstract class FilterFunctionCompare<L extends ExchangeableLeaf<L>> {
 	 *         targetNode
 	 */
 	public static Map<Path, FactAddress> equals(final Node targetNode, final PathNodeFilterSet pathFilter) {
-		final HashSet<Path> collectedPaths = PathCollector.newHashSet().collectAllInLists(pathFilter).getPaths();
+		final Set<Path> collectedPaths =
+				PathCollector.newHashSet().collectAllInLists(pathFilter).getPaths().stream()
+						.map(p -> p.getJoinedWith().iterator().next()).distinct().collect(toSet());
 		if (targetNode.getMemory().getTemplate().length != collectedPaths.size()) {
 			return null;
 		}
@@ -596,8 +598,7 @@ public abstract class FilterFunctionCompare<L extends ExchangeableLeaf<L>> {
 		{
 			// get representatives for the joined-with-sets (i.e. the edges) and group by nodes
 			final Map<Node, Set<Path>> pathSetByNode =
-					collectedPaths.stream().map(p -> p.getJoinedWith().iterator().next()).distinct()
-							.collect(groupingBy(s -> s.getCurrentlyLowestNode(), toSet()));
+					collectedPaths.stream().collect(groupingBy(s -> s.getCurrentlyLowestNode(), toSet()));
 			// now we have a set of components consisting of sets of representatives
 			final Collection<Set<Path>> components = pathSetByNode.values();
 			final List<Range> ranges = new ArrayList<>(components.size());
