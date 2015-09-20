c25af4db71324c34911349cc58ee8dd69682259b
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2015-09-20 12:06:53 +0200

    Partitions: removing loopkup map keys

--- src/org/jamocha/dn/compiler/ecblocks/FilterInstancePartition.java
+++ src/org/jamocha/dn/compiler/ecblocks/FilterInstancePartition.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2015 The Jamocha Team
- *
- *
+ * 
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.jamocha.org/
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -72,8 +72,13 @@ class FilterInstancePartition extends Partition<FilterInstance, FilterInstancePa
 	@Override
 	public boolean remove(final FilterInstanceSubSet s) {
 		final boolean removed = super.remove(s);
-		if (removed)
-			filterLookup.get(s.filter).remove(s);
+		if (removed) {
+			final Set<FilterInstanceSubSet> set = filterLookup.get(s.filter);
+			set.remove(s);
+			if (set.isEmpty()) {
+				filterLookup.remove(s.filter);
+			}
+		}
 		return removed;
 	}
 }
