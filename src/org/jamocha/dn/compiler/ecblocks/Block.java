5c619a15b91af82c366b1697658bf12058eb4cd8
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2015-09-20 13:25:46 +0200

    Block: nullsafe removeFilterInstanceSubSet

--- src/org/jamocha/dn/compiler/ecblocks/Block.java
+++ src/org/jamocha/dn/compiler/ecblocks/Block.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2015 The Jamocha Team
- *
- *
+ * 
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.jamocha.org/
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -299,7 +299,7 @@ public class Block {
 			return false;
 		}
 		flatFilterInstances.removeAll(subset.elements.values());
-		if (filterInstancePartition.lookupByFilter(subset.filter).isEmpty()) {
+		if (!filterInstancePartition.filterLookup.containsKey(subset.filter)) {
 			filters.remove(subset.filter);
 		}
 		return true;
